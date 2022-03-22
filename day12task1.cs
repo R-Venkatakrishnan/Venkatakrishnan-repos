5a143368895950f10a2a9ea40b78e212631e1c71
R-Venkatakrishnan <101104446+R-Venkatakrishnan@users.noreply.github.com>
2022-03-22 16:17:03 +0530

    Add files via upload

---
+++ day12task1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace ConsoleApp1
+{
+    class day12task1
+    {
+        public static void Main()
+        {
+
+            FileStream fs = new FileStream("day121.txt", FileMode.OpenOrCreate, FileAccess.ReadWrite);
+            StreamWriter bw = new StreamWriter(fs);
+            List<string> str = new List<String>();
+            str.Add("ice cream");
+            str.Add("jangree");
+            str.Add("juice");
+            str.Add("milkshake");
+            str.Add("toffelle");
+            foreach (string d in str)
+            {
+                bw.Write("\n" + d);
+            }
+            bw.Flush();
+            bw.Close();
+            fs.Close();
+            FileStream fs1 = new FileStream("day121.txt", FileMode.OpenOrCreate, FileAccess.ReadWrite);
+            StreamReader sr = new StreamReader(fs1);
+            String s = sr.ReadToEnd();
+            sr.Close();
+            fs1.Close();
+            Console.WriteLine(s);
+            FileInfo fi = new FileInfo("d:\\c#\\file\\Dessert.txt");
+            Console.WriteLine(fi.Length);
+            Console.WriteLine(fi.CreationTime);
+            Console.Read();
+        }
+    }
+}
