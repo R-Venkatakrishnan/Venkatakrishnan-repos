36672e0dc034a1d99716352246ef2d26e9d71c0c
R-Venkatakrishnan <101104446+R-Venkatakrishnan@users.noreply.github.com>
2022-04-08 17:26:30 +0530

    Add files via upload

---
+++ Class9.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+
+
+	class class9
+	{
+
+
+		public static void Main()
+		{
+			int eye = 52, Legs = 80;
+
+			int count = 0;
+			int count2 = 0;
+
+			count = 2 * (eye) - (Legs);
+			count = count / 2;
+
+			Console.WriteLine("No of Ducks:" + count);
+
+			count2 = 26 - count;
+			Console.WriteLine("No of Goats:" + count2);
+		}
+	}
+
+}
